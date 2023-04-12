fda82e3642e6ef53483a8408f6d17e9124c671ac
preguntasinteractivas22 <108531117+preguntasinteractivas22@users.noreply.github.com>
2023-04-12 17:02:40 -0300

    Add files via upload

---
+++ panel/vendor/fusonic/opengraph/examples/consume_website.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\HttpClient\NativeHttpClient;
+use Symfony\Component\HttpClient\Psr18Client;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+// Initialize new Psr\HttpClient implementation. This example uses Symfony's implementation from the symfony/http-client
+// package but you can use any implementation provided by your framework of choice.
+$client = new Psr18Client(new NativeHttpClient([ "headers" => [ "User-Agent" => "facebookexternalhit/1.1" ] ]));
+
+// Create a new crawler
+$crawler = new Fusonic\OpenGraph\Consumer($client, $client);
+
+// Crawl the desired URL and retrieve a Fusonic\OpenGraph\Object in response
+$object = $crawler->loadUrl("https://github.com");
+
+var_dump($object);
